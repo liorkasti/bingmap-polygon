297129855a339e4c83bc7568a95ee9c07ca874ee
Lior Kastenbaum <liorkasti@gmail.com>
2021-10-11 20:11:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import BingMapsReact from "bingmaps-react";
+import { useForm, FormProvider, useFormContext } from "react-hook-form";
+
+import Form from "./components/Form/Form"
 
 function App() {
+
+  const [coordinates, setCoordinates] = useState([]);
+  useEffect(() => {
+    console.log('coordinates: ', coordinates)
+  }, [coordinates])
+
+  // window.Microsoft = window.Microsoft || {};
+  const map = document.getElementById('myMap', {});
+  console.log('map: ', map)
+
+
+  const appendLocationHandler = async (locationNode) => {
+    setCoordinates(locationNode);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Form className="form"
+        coordinates={coordinates}
+        onAddLocation={appendLocationHandler}
+      />
+      <BingMap className="map" />
     </div>
+  );
+}
+
+function BingMap() {
+  return (
+    <BingMapsReact
+      bingMapsKey="Au_s6stObqYe5mrDRVJvSf1lJ3bfuiHC1V_xKVGlq1vyHFuREUOuay9fesgsSfTb"
+      height="500px"
+      mapOptions={{
+        navigationBarMode: "square",
+      }}
+      width="60%"
+      viewOptions={{
+        center: { latitude: 32.321, longitude: -80.757 },
+        zoom: 4
+        // mapTypeId: "grayscale",
+
+      }}
+    />
   );
 }
 
---
+++ src/components/Form/Form.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { useForm, FormProvider, useFormContext } from "react-hook-form";
+import Test from "./Test";
+
+import "./styles.css";
+
+const Form = (props) => {
+    useEffect(() => {
+        console.log(props)
+        console.log(selectedLocation)
+    }, [])
+
+    const [selectedLocation, setSelectedLocation] = useState([]);
+    const [cityMode, setCityMode] = useState(false);
+    const [placeName, setPlaceName] = useState("");
+    const [latitude, setLatitude] = useState();
+    const [longitude, setLongitude] = useState();
+    const [polygonIndex, setPolygonIndex] = useState(0);
+
+    const methods = useForm();
+    const { register, handleSubmit, watch, setValue, formState: { errors } } = useForm();
+
+    const onSubmit = (data) => {
+        // alert(JSON.stringify(data));
+        // console.log('data: ' + JSON.stringify(data));
+        setLatitude(data.latitude);
+        setLongitude(data.longitude);
+        // setPolygonIndex(Math.random().toString(36).substr(2, 5))
+        setPolygonIndex(polygonIndex + 1)
+        props.onAddLocation([...props.coordinates, {
+            polygonIndex: polygonIndex,
+            latitude: latitude || 32.321,
+            longitude: longitude || -80.757
+        }])
+        // console.log('register: ' + register);
+        // .preventDefault()
+    };
+
+    const handleInputMode = (value) => {
+        console.log('cityMode: ', value);
+        setPlaceName(placeName)
+        setCityMode(!value);
+    }
+
+    return (
+        <FormProvider {...methods}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <h1>Coordinates form:</h1>
+                <div className="search-type">
+                    <input
+                        type="radio"
+                        {...register("cityMode")}
+                        defaultValue="yes"
+                        value={cityMode}
+                        checked={!cityMode}
+                        onClick={() => handleInputMode(cityMode)}
+                    />
+                    <label htmlFor="cityMode">Add by Coords</label>
+                </div>
+                <div className="search-type">
+                    <input
+                        type="radio"
+                        {...register("city")}
+                        value={cityMode}
+                        checked={cityMode}
+                        onClick={() => handleInputMode(cityMode)}
+                    />
+                    <label htmlFor="city">Add by City</label>
+                </div>
+
+                {cityMode ?
+                    <div>
+                        <label htmlFor="placeName">Place Name</label>
+                        <input {...register("placeName")} placeholder={`${placeName ? placeName : 'Place Name'}`} /* defaultValue={placeName} */ />
+                    </div>
+                    :
+                    <div>
+                        <label htmlFor="latitude">Latitude</label>
+                        <input {...register("latitude")} placeholder="Enter Latitude" />
+
+                        <label htmlFor="longitude">Longitude</label>
+                        <input {...register("longitude")} placeholder="Enter Longitude" />
+                    </div>
+
+                }
+                {/* <label>Nested Input</label>
+                <Test /> */}
+                <input type="submit" value="Add Coords"
+                    onClick={() =>
+                        setValue("Add Coords", "true", {
+                            shouldValidate: true,
+                            shouldDirty: true
+                        })
+                    } />
+            </form>
+        </FormProvider>
+    );
+}
+export default Form;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Form />, rootElement);
